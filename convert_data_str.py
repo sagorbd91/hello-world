729aa17bc363b99651cc0e7a6f4a7c2d241f1143
sagorbd91 <sagor.ghosh@dal.ca>
2020-02-19 13:11:17 -0400

    list to dictionary conversion

--- convert_data_str.py
+++ convert_data_str.py
@@ -19,3 +19,43 @@ print(lsty)
 tupz = tuple(lsty)
 print(tupz)
 
+# convert tuple to list
+
+lsta = list(tupz)
+print(lsta)
+
+# Convert list of tuple to dictionary
+
+t = [("a", 1),("b", 2)]
+for i in t:
+    print(i)
+    
+dic = dict(t)
+print(dic)
+
+##############################
+
+# Enumerate list and dictionary
+
+example = ["north", "south", "east", "west", "left", "right"]
+
+for i in range(len(example)):
+        print(i, example[i])
+
+for i, e in enumerate(example):
+    print(i,e)
+
+new_dict = dict(enumerate(example))
+print(new_dict)
+
+
+###############################
+# Convert Dictionary from a list and count them
+
+lis = [12,10,29,181,66,78]
+my_dict ={}
+for i in range(len(lis)):
+        my_dict[lis[i]] = lis.count(lis[i])
+
+print(my_dict)
+
